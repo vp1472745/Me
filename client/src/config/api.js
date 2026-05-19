ca6c0fb6ef4d6ec243b53fbde716baea30c1bd73
vineet-pancheshwar_rdpl <vineet.pancheshwar@rajdigital.com>
2026-05-19 15:50:24 +0530

    feat: Configure API base URL for development and production environments

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true,
+  baseURL: BASE_URL,
+timeout: 180000 ,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  maxContentLength: Infinity,
+  maxBodyLength: Infinity,
 });
 
 // ==========================
