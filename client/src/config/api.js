631f4673249780541a5ecd9495a30460643119fb
vp1472745 <vineetpancheshwar1611@gmail.com>
2026-06-24 01:27:17 +0530

    a

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL ||"https://me-1-u6w8.onrender.com";
+const BASE_URL = import.meta.env.VITE_API_URL ||"https://me-1-u6w8.onrender.com/api";
 
 const API = axios.create({
   baseURL: BASE_URL,
