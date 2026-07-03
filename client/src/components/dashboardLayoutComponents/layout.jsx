58fe86ce864e1c8057ff7f3705bd75e5d1f3c346
vp1472745 <vineetpancheshwar1611@gmail.com>
2026-07-04 02:34:11 +0530

    remove padding

--- client/src/components/dashboardLayoutComponents/layout.jsx
+++ client/src/components/dashboardLayoutComponents/layout.jsx
@@ -219,7 +219,7 @@ function Layout({ roleType }) {
         </header>
 
         {/* Scrollable content */}
-        <main className="flex-1 overflow-y-auto p-4 sm:p-6">
+        <main className="flex-1 overflow-y-auto  sm:p-6">
           <Outlet />
         </main>
       </div>
